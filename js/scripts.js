ebaa72e864139f364fbabe07f4b21375b8121ced
Stephen Zhao <mail@zhaostephen.com>
2016-10-11 22:03:45 -0400

    fixed some browser-specific issues
    - still does not work in Edge
    - not intended to support any version of IE
    - works well in firefox and chrome (latests)

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ var b_homeNavToggleUp_isShow = false;
 var homeNavToggleUp_A_onClick = function(e) {
   e.preventDefault();
   if (b_homeNavToggleUp_isShow) {
-    $(".TopNav_Div_Home").slideUp(1000, "easeOutExpo", function() {
+    $(".TopNav_Div_Home").stop(true, true).slideUp(1000, "easeOutExpo", function() {
       b_homeNavToggleUp_isShow = false;
     });
     // $(".Page_DivBackground").css({filter: "none", "-webkit-filter": "none"}, 1000, function() {
     // });
   }
   else {
-    $(".TopNav_Div_Home").slideDown(1000, "easeOutExpo", function() {
+    $(".TopNav_Div_Home").stop(true, true).slideDown(1000, "easeOutExpo", function() {
       b_homeNavToggleUp_isShow = true;
     });
     // $(".Page_DivBackground").css({filter: "blur(2px)", "-webkit-filter": "blur(2px)"}, 1000, function() {
@@ -21,12 +21,13 @@ var homeNavToggleUp_A_onClick = function(e) {
 var homeNavToggleDown_A_onClick = function(e) {
   e.preventDefault();
   var href = e.currentTarget.href;
+  $(".TopNav_Div").stop(true, true);
   if (b_homeNavToggleUp_isShow) {
     $(".TopNav_Div").slideUp(800, "easeOutExpo", function() {
       b_homeNavToggleUp_isShow = false;
     });
   }
-  $(".Home_DivContainer").animate({position: "relative", marginTop: "-260%"}, 600, "easeOutExpo", function() {
+  $(".Home_DivContainer").stop(true, true).animate({position: "relative", marginTop: "-260%"}, 600, "easeOutExpo", function() {
     window.location.href = href;
   });
 }
