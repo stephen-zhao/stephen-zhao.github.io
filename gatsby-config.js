f0e3f302f15d8882a8c0ef96ac9f1a3a76e14614
Stephen Zhao <mail@zhaostephen.com>
2019-10-06 01:49:24 -0400

    add typescript support

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // TODO: add personal icon
       },
     },
+    `gatsby-plugin-typescript`,
     // TODO: enable offline functionality
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
