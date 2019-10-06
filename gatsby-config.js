56953891ce25eebffc63f52022998eae3675760c
Stephen Zhao <mail@zhaostephen.com>
2019-10-06 18:17:45 -0400

    add typescript-css-modules gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,9 +24,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // TODO: add personal icon
+        //icon: `src/images/gatsby-icon.png`, // TODO: add personal icon
       },
     },
+    `gatsby-transformer-typescript-css-modules`,
     `gatsby-plugin-typescript`,
     // TODO: enable offline functionality
     // this (optional) plugin enables Progressive Web App + Offline functionality
